30e9f3133e5f1cab2ed7e54f2a3a100c87e3461b
Anandhakrishnan Balasubramanian <anandhakrishnan_b@outlook.com>
2017-09-11 14:11:35 +0530

    updated the endpoints

--- www/js/app/settings.js
+++ www/js/app/settings.js
@@ -1,4 +1,16 @@
-﻿var TalentSearch = TalentSearch || {};
+var TalentSearch = TalentSearch || {};
+
 TalentSearch.Settings = TalentSearch.Settings || {};
-TalentSearch.Settings.signInUrl = "http://www.mocky.io/v2/59b045681000002e08db2171"; 
+TalentSearch.Settings.BaseURL = "http://localhost:8080/accenture-campus-placement/"
+
+TalentSearch.Settings.signInUrl = TalentSearch.Settings.BaseURL+"login";
+TalentSearch.Settings.addVenueUrl = TalentSearch.Settings.BaseURL+"venueDetail/add";
+TalentSearch.Settings.addUserUrl = TalentSearch.Settings.BaseURL+"user/add";
+TalentSearch.Settings.addPanelUrl = TalentSearch.Settings.BaseURL+"selectionPanelDetail/add";
+TalentSearch.Settings.addSelectionUrl = TalentSearch.Settings.BaseURL+"selectionDetail/add";
+TalentSearch.Settings.loadVenuesUrl = TalentSearch.Settings.BaseURL+"venueDetails";
+TalentSearch.Settings.loadPanelsUrl = TalentSearch.Settings.BaseURL+"selectionPanelDetails";
+TalentSearch.Settings.loadSelectionsUrl = TalentSearch.Settings.BaseURL+"selectionDetails";
+TalentSearch.Settings.loadUserUrl = TalentSearch.Settings.BaseURL+"users";  
+ 
 TalentSearch.Settings.sessionTimeoutInMSec = 600000
